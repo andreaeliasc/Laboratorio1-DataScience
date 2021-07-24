6feb1370a3682cdff8f5f37b5edd643518c94599
andreaeiasc <andrea.elias16@gmail.com>
2021-07-23 19:40:14 -0600

    Analisis Exploratorio

---
+++ Lab1.R
@@ -0,0 +1,48 @@
+#Andrea Elias
+#Diego Estrada 
+
+library(dplyr)
+library(tidyr)
+library(rpart)
+library(caret)
+library(tree)
+library(rpart.plot)
+library(randomForest)
+library(dplyr)
+library(tidyr)
+library(cluster)
+library(e1071)
+library(mclust)
+library(fpc)
+library(NbClust)
+library(factoextra)
+library(cluster)
+library(e1071)
+library(mclust)
+library(fpc)
+library(NbClust)
+library(factoextra)
+library(rpart)
+
+# Analisis Exploratorio
+train<- read.csv("train.csv", stringsAsFactors = FALSE)
+test<- read.csv("test.csv", stringsAsFactors = FALSE)
+train<-train[1:1460,]
+
+glimpse(train[1:10,])
+
+#graficas de correlacion de las variables que se utilizan vs el precio de compra
+scatter.smooth(train$LotFrontage, train$SalePrice)
+scatter.smooth(train$LotArea, train$SalePrice)
+scatter.smooth(train$GrLivArea, train$SalePrice)
+scatter.smooth(train$YearBuilt, train$SalePrice)
+scatter.smooth(train$BsmtUnfSF, train$SalePrice)
+scatter.smooth(train$TotalBsmtSF, train$SalePrice)
+scatter.smooth(train$X1stFlrSF, train$SalePrice)
+scatter.smooth(train$GarageYrBlt, train$SalePrice)
+scatter.smooth(train$GarageArea, train$SalePrice)
+scatter.smooth(train$YearRemodAdd, train$SalePrice)
+
+scatter.smooth(train$TotRmsAbvGrd, train$SalePrice)
+scatter.smooth(train$MoSold, train$SalePrice)
+scatter.smooth(train$OverallQual, train$SalePrice)
